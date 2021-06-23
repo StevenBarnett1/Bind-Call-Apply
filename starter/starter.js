5cb3a267438093d130b5a6f1c400334970d8bb2c
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-06-23 08:54:03 -0700

    second commit

--- starter/starter.js
+++ starter/starter.js
@@ -1,2 +1,25 @@
 class Employee {
+    constructor(name,occupation){
+        this.name = name;
+        this.occupation = occupation;
+
+    }
+    sayName(){
+        console.log(this.name)
+    }
+    sayOccupation(){
+        console.log(this.occupation)
+    }
+
 }
+
+
+let employee = new Employee("Ben","President")
+
+let boundName = employee.sayName.bind(employee)
+
+let boundOccupation = employee.sayOccupation.bind(employee)
+
+globalThis.setTimeout(boundName,2000)
+
+globalThis.setTimeout(boundOccupation,6000)
